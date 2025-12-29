67437be891d84c3e024cd99e2d007fc1af89e7cb
LeonardoHSSoares <mascoty2005@gmail.com>
2025-12-29 12:52:33 -0300

    🗑️ chore(Abstract Factory): remover arquivos e configurações desnecessárias

--- Abstract Factory/src/br/com/leonardo/fabricadecarros/fabricas/IFabricaDeCarros.java
+++
@@ -1,7 +0,0 @@
-package br.com.leonardo.fabricadecarros.fabricas;
-
-import br.com.leonardo.fabricadecarros.carros.ICarro;
-
-public interface IFabricaDeCarros {
-    ICarro criarCarro(String modelo,int ano,String combustivel);
-}
