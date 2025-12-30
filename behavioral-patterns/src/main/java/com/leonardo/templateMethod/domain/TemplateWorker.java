86f891bb82c3fcb929bb0f129896cb727c5c07ec
LeonardoHSSoares <mascoty2005@gmail.com>
2025-12-30 07:30:31 -0300

    🐞 fix:

--- behavioral-patterns/src/main/java/com/leonardo/templateMethod/domain/TemplateWorker.java
+++ behavioral-patterns/src/main/java/com/leonardo/templateMethod/domain/TemplateWorker.java
@@ -4,14 +4,14 @@ public abstract class TemplateWorker {
 
     // Template Method
     public final void run() {
-        
+        work();
     }
     public void work() {
         startWork();
         takeBreak();
         endWork();
     }
-
+    
     protected abstract void startWork();
     protected abstract void takeBreak();
     protected abstract void endWork();
